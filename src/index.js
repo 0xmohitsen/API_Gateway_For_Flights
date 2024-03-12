08bd415a35a15e4a22cd9de8ccedcedd9a5dfe92
Mohitsen11 <mohitsen0102@gmail.com>
2024-03-12 19:56:27 +0530

    Added rate-limiter to keep the server healthy

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
+const { rateLimit } = require('express-rate-limit');
 
 const app = express();
 
+const limiter = rateLimit({
+	windowMs: 2 * 60 * 1000, // 2 minutes
+	limit: 2, // Limit each IP to 2 requests per `window` (here, per 2 minutes).
+})
+app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
