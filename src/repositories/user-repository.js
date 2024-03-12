6773bb9fba8d44fa1e5c3fb8eb639c7b3438e202
Mohitsen11 <mohitsen0102@gmail.com>
2024-03-13 01:16:05 +0530

    Removed unnecessary import

--- src/repositories/user-repository.js
+++ src/repositories/user-repository.js
@@ -1,6 +1,5 @@
 const CrudRepository = require('./crud-repository');
 const { User } = require('../models');
-const AppError = require('../utils/error/app-error');
 
 class UserRepository extends CrudRepository{
     constructor(){
